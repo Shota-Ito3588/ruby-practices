34316e89c658a6c1c44f51d8f4bb44cebc8f8f21
Shota <s.ito@wiz-net.jp>
2020-12-29 22:09:06 +0900

    calendar課題の再提出

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,24 +1,36 @@
-require "date"
+require "date" #dateクラスのデータを受け取る
+require 'optparse' #optparseクラスのデータを受け取る
+day = Date.today #今日の情報
+options = ARGV.getopts("y:", "m:") #引数付きショートネームオプションの指定
 
-head = Date.today.strftime("%B %Y") #今月の月と西暦
-year = Date.today.year
-mon = Date.today.mon
-firstday_wday = Date.new(year,mon, 1).wday #今月初めの曜日
-lastday_wday = Date.new(year,mon, -1).day #今月の最終日
+if options["y"] #もし"y"に値が入力されたら
+  year = options["y"].to_i #入力値をyear変数に代入
+else
+  year = day.year #入力されないと今年のデータになる
+end
+
+if options["m"] #もし"m"に値が入力されたら
+  month = options["m"].to_i #入力値を今年をmonth変数に代入
+else
+  month = day.mon #入力されないと今月のデータになる
+end
+
+head = Date.today.strftime("%B, %Y") #今月の月と西暦を取得
+year = Date.today.year #今月の西暦
+mon = Date.today.mon #今日の月
+firstday = Date.new(year,mon, 1).day #今月初めの曜日
+lastday = Date.new(year,mon, -1).day #今月の最終日
 week = %w(Su Mo Tu We Th Fr Sa) #配列を作る表記
 
 puts head.center(20) #頭部分を中央寄せ
 puts week.join(" ") #曜日の間隔を開けるように表示
-print "  " * firstday_wday #１日までの空白を表示
+print "  " * firstday #１日までの空白を表示
 
-wday = firstday_wday
-(1..lastday_wday).each do |date| #(1..last...)で指定した範囲で繰り返し
+week = firstday
+(1..lastday).each do |date| #(1..last...)で指定した範囲で繰り返し
   print date.to_s.rjust(2) + " " #日付を右寄せで表示
-  wday = wday+1
-  if wday%7==0 #7の倍数で改行
+  week = week + 1
+  if week % 7 == 0 #土曜で改行する
     print "\n"
   end
-end
-if wday%7!=0
-  print "\n"
 end
